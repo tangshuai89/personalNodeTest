496121620636d72b622a5ee3e788ebcd60ddc008
Andrew Clearness <blacksweaters@live.cn>
2018-08-06 17:27:18 +0800

    临时提交

---
+++ test/help_caiguohang.js
@@ -0,0 +1,37 @@
+var strurl = "http://117.131.17.174:8000/pre/api/v1/cms/content-selectors";
+
+$(document).ready(function() {
+	initiliazeStats();
+
+	$(".dev, .pre, .online").click(function() {
+		const stats = spiderNose($(this).attr("class"));
+		changeDev(stats[0]);
+		changeStyle(stats[0]);
+	});
+});
+
+function initiliazeStats() {
+	let sessionStorageOfStats = sessionStorage.getItem("stats");
+	changeStyle(sessionStorageOfStats);
+}
+
+function spiderNose(classList) {
+	return classList.match(/dev/g) || classList.match(/online/g) || classList.match(/pre/g);
+}
+
+function changeDev(dev) {
+	sessionStorage.setItem("stats", dev);
+}
+
+function changeStyle(dev) {
+	$(".huanli, .online").removeClass('active');
+	let activeClassName = '';
+	if (dev == "pre") {
+		activeClassName = 'pre';
+	} else if (dev == "dev") {
+		activeClassName = 'dev';
+	} else if (dev == "online") {
+		activeClassName = 'online';
+	}
+	$(dev).addClass("active");
+}
